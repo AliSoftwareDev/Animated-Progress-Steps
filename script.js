c3336c66f067c0f4f746f0e88537aad1582f7efd
Ali İslam <bgane8181@gmail.com>
2026-03-25 22:00:06 +0300

    İlk Commit

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-const panels = document.querySelectorAll(".panel");
+const progress = document.getElementById("progress");
+const circles = document.querySelectorAll(".circle");
+const next = document.getElementById("next");
+const previous = document.getElementById("previous");
 
-panels.forEach( panel => {
-    panel.addEventListener("click", () => {
+let currentActive = 0;
 
-        removeActive();
-        panel.classList.add("active");
-    })
+next.addEventListener("click", () => {
+        currentActive++;
+        if(currentActive > circles.length) {
+                currentActive = circles.length;
+        }
+
+        update();
 })
 
-function removeActive() {
-    panels.forEach( panel => {
-        panel.classList.remove("active");
-    })
+previous.addEventListener("click", () => {
+        currentActive--;
+        if(currentActive < 1) {
+                currentActive = 1;
+        }
+        update();
+});
+
+function update() {
+        circles.forEach((circles, index) => {
+                if(index < currentActive) {
+                        circles.classList.add("active");
+                } else {
+                        circles.classList.remove("active");
+                }
+        })
+
+        const actives = document.querySelectorAll(".active");
+
+        progress.style.width = (actives.length -1) / (circles.length -1) * 100 + "%";
+
+        if(currentActive === 1){
+                previous.disabled = true;
+        } else if(currentActive === circles.length) {
+                next.disabled = true;
+        } else {
+                previous.disabled = false;
+                previous.disabled = false;
+        }
 }
